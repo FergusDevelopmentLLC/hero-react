3793b549df30bd9601260b88fa1e06345a377848
Will Carter <will.carter@fergusllc.com>
2020-12-27 09:10:07 -0700

    refactor

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
 import './App.css'
 import Hero from './components/Hero'
 
-
-
-function App() {
-  // const spriteSheetUrl = "https://res.cloudinary.com/fergusdev/image/upload/v1609029014/hero/redskull_r3wvcc.png"
-  const spriteSheetUrl = "https://res.cloudinary.com/fergusdev/image/upload/v1609029014/hero/colossus_fejryj.png"
-
+const App = () => {
   return (
     <div className="App">
-      <Hero 
-          speed={ 100 } 
-          spriteSheetUrl={ spriteSheetUrl }
-          /> 
+      <Hero type="pirate" /> 
     </div>
-  );
+  )
 }
 
 export default App
