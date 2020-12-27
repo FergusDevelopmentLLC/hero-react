6ec83d915aac51ccf8aa35677eb73e73ee1b6555
Will Carter <will.carter@fergusllc.com>
2020-12-26 18:06:13 -0700

    move spriteSheetUrl to const

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import './App.css'
 import Hero from './components/Hero'
 
 
+
 function App() {
+  // const spriteSheetUrl = "https://res.cloudinary.com/fergusdev/image/upload/v1609029014/hero/redskull_r3wvcc.png"
+  const spriteSheetUrl = "https://res.cloudinary.com/fergusdev/image/upload/v1609029014/hero/colossus_fejryj.png"
+
   return (
     <div className="App">
       <Hero 
-          speed={ 200 } 
-          spriteSheetUrl="https://res.cloudinary.com/fergusdev/image/upload/v1609029014/hero/colossus_fejryj.png"
+          speed={ 100 } 
+          spriteSheetUrl={ spriteSheetUrl }
           /> 
     </div>
   );
