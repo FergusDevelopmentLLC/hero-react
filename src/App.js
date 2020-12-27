090751869af02911c0ebcae82bdfc2ff2c420e13
Will Carter <will.carter@fergusllc.com>
2020-12-27 13:32:50 -0700

    provide random action to pirate

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
 import './App.css'
+import { useEffect, useState } from 'react'
 import Pirate from './components/Pirate'
 import Hero from './components/Hero'
 
 const App = () => {
+
+  const actions = ["idle", "walk", "run", "jump", "attack", "hurt", "die"]
+
+  const [action, setAction] = useState(actions[1])
+
+  useEffect(() => {
+
+    setInterval(() => {
+      console.log('update state')
+      let randomAction = actions[actions.length * Math.random() | 0]
+      setAction(randomAction)
+    }, 5000)
+
+    return () => {
+      console.log('unmount')
+    }
+
+  }, [])
+
   return (
+
     <div className="App">
-     <Pirate type="pirate-idle" /><Hero />
-      
+      <Pirate action={ action } /><Hero />
     </div>
+
   )
 }
 
