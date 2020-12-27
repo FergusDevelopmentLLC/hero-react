94edab9b0686d5052a2886263c61094a788dee21
Will Carter <will.carter@fergusllc.com>
2020-12-26 17:51:03 -0700

    Hero component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import Hero from './components/Hero'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Hero 
+          speed={ 200 } 
+          spriteSheetUrl="https://res.cloudinary.com/fergusdev/image/upload/v1609029014/hero/colossus_fejryj.png"
+          /> 
     </div>
   );
 }
 
-export default App;
+export default App
